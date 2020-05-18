8c12e03d0cba9c8cf176945108264b485a4dbcc7
Zemlyanova-Victoria <zemlyanova-v@list.ru>
2020-05-18 20:57:24 +0500

    first commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Convert;
+using static System.Console;
+using System.Collections;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add('L');
+            list.Add(54);
+            list.Add('O');
+            list.Add(5);
+            list.Add('b');
+            list.Add(64);
+            list.Add(-98);
+            list.Add(0);
+            list.Add('V');
+            list.Add(99);
+            list.Add('x');
+            list.Add(9);
+            list.Add(2);
+            list.Add('E');
+
+            ArrayList list2 = new ArrayList();
+            WriteLine("Chisla: ");
+            for (int i = 0; i < list.Count; i++)
+            {
+                int n = 0;
+                string k = list[i] + "";
+                bool r = int.TryParse(k, out n); ;
+
+                if (r == false)
+                {
+                    if (k == k.ToUpper())
+                    {
+                        list2.Add(list[i]);
+                        list.RemoveAt(i);
+                    }
+                    else list.RemoveAt(i);
+                }
+            }
+            foreach (object o in list)
+            {
+                WriteLine(o);
+            }
+            WriteLine("Zaglavnyye bykvy: ");
+            foreach (object o in list2)
+            {
+                WriteLine(o);
+            }
+            ReadLine();
+        }
+    }
+}
+
+        
+    
+
